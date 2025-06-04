e09ee7d240b25f164667b40a1b2be8ddba7e94c8
Luca Williams <ljwilliams@colgate.edu>
2025-06-04 13:53:57 -0400

    parsing through comments pages

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,20 @@
 import requests
 import bs4 as BeautifulSoup
 
+baseURL = "https://www.regulations.gov/search/comment"
+
+params = {
+    "sortBy" : "postedDate",
+    "sortDirection" : "desc",
+    "pageNumber" : 1
+}
+
+while True:
+    print(f"Fetching data from page {params['pageNumber']}...")
+    page = requests.get(baseURL, params=params)
+    if (page.status_code != 200):
+        print("Connection Failed")
+        break
+    params['pageNumber'] += 1
+
+print("No errors")
