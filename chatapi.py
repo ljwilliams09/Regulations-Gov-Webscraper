1bd71e63c70d289546ea6d1cabe002fffb0b36dd
Luca Williams <ljwilliams@colgate.edu>
2025-06-19 17:57:24 -0400

    changes

--- chatapi.py
+++ chatapi.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI(api_key=os.getenv("CHAT_API_KEY"))
 
-def api_call(comment_title):
+def api_call(comment_title, comID):
     prompt = f"""Does the following comment title contain an affiliation or organization for the commenter? Respond only with 1 for Yes, or 0 for No.
 
-    Title: {comment_title}
+    Title: {comment_title}, Comment ID: {comID}
     """
     response = client.chat.completions.create(model="gpt-4.1",
     messages=[{
@@ -24,7 +24,7 @@ def api_call(comment_title):
 def main():
     # Variables and Paths
     titles = "500_sample.csv"
-    results = "samplecoding.csv"
+    results = "chatGPT_500_coding.csv"
 
     with open(titles, 'r',encoding='utf-8') as f:
         reader = csv.reader(f)
@@ -32,8 +32,7 @@ def main():
         with open(results, 'a', newline='', encoding='utf-8') as r:
             writer = csv.writer(r)
             for row in reader:
-                comment_title = row[1]
-                response = api_call(comment_title)
+                response = api_call(row[1], row[0])
                 print("Response: ", response)
                 writer.writerow([row[1], response])
 main()
