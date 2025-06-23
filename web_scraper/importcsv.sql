0b8dbdb7f125eec67d88cc645c0dfbd4447b9f25
Luca Williams <ljwilliams@colgate.edu>
2025-06-23 14:53:26 -0400

    Rename comments table to comments_v2 and update CSV file path for data import

--- web_scraper/importcsv.sql
+++ web_scraper/importcsv.sql
@@ -1,4 +1,4 @@
-CREATE TABLE comments (
+CREATE TABLE comments_v2 (
     id TEXT,
     documentType TEXT,
     lastModifiedDate TEXT,
@@ -9,7 +9,7 @@ CREATE TABLE comments (
     postedDate TEXT
 )
 
-COPY COMMENTS
-FROM '/Users/lucawilliams/Desktop/comment_data.csv'
+COPY comments_v2
+FROM '/Users/lucawilliams/Desktop/comment_data_v2.csv'
 DELIMITER ','
 CSV HEADER
