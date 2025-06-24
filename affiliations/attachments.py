c689ac29d69524353a1349b0ce7d410882bd68af
Luca Williams <ljwilliams@colgate.edu>
2025-06-24 11:10:16 -0400

    Enhance error handling in linker function: implement rate limiting for API requests and improve exception messages

--- affiliations/attachments.py
+++ affiliations/attachments.py
@@ -1,23 +1,25 @@
 from openai import OpenAI
 import os
 import requests
+import time
 
 def linker(comment_id):
-    url = "https://api.regulations.gov/v4/comments/" + comment_id
+    url = "https://api.regulations.gov/v4/comments/attachments" + comment_id
     params = {
-        "api_key" : "RWhAaanqXHMC89fGk755BO70rN8ygv1txMawAG3a"
+        "api_key" : os.getenv("REG_GOV_API_KEY_LW")
     }
-
-    comment_page = requests.get(url, params=params) # get the comment
-    if comment_page.status_code != 200:
-        raise Exception(f"Failed to access the comment page for the comment {comment_id}, with staus code {comment_page.status_code}")
+    while True:
+        comment_page = requests.get(url, params=params) # get the comment
+        if comment_page.status_code != 200:
+            if comment_page.status_code == 429:
+                time.sleep(3600)
+                continue
+            raise Exception(f"Failed to access the comment page for the comment {comment_id}, with staus code {comment_page.status_code}")
     
-    link = (comment_page.json())["data"]["relationships"]["attachments"]["links"]["related"]
-
-    link_page = requests.get(link, params=params)
-    if link_page.status_code != 200:
-            raise Exception(f"Failed to access the link page for the comment {comment_id}, with status code {link_page.status_code}")
-
+        link = (comment_page.json())["data"]["relationships"]["attachments"]["links"]["related"]
+        link_page = requests.get(link, params=params)
+        if link_page.status_code != 200:
+                raise Exception(f"Failed to access the link page for the comment {comment_id}, with status code {link_page.status_code}")
     data = link_page.json()
     if not data["data"]:
         return  "N/A" 
