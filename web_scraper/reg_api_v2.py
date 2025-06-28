b09c2e4413cbc0336467b5caa33040dd31b0d5bb
Luca Williams <ljwilliams@colgate.edu>
2025-06-28 11:32:31 -0400

    Refactor date_format_param to accept a differential hours parameter and implement test_reset_point for improved API response handling

--- web_scraper/reg_api_v2.py
+++ web_scraper/reg_api_v2.py
@@ -9,9 +9,9 @@ from logger_config import logger
 def year_range(year):
     return f'{year}-01-01', f'{year}-12-31'
 
-def date_format_param(last_date):
+def date_format_param(last_date, diff=6):
     date = datetime.strptime(last_date, "%Y-%m-%dT%H:%M:%SZ")
-    return (date - timedelta(hours=6)).strftime("%Y-%m-%d %H:%M:%S")
+    return (date - timedelta(hours=diff)).strftime("%Y-%m-%d %H:%M:%S")
 
 def normalize_date(date):
     date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ")
@@ -20,13 +20,38 @@ def normalize_date(date):
 def clean_text(text):
     return text.replace('\r', ' ').replace('\n', ' ').strip()
 
+def test_reset_point(url, params, lastModifiedDate, totalElements): 
+    hours = 1
+    logger.info(f"Testing Reset Point with {hours} hour(s) differential")
+    logger.info(f"totalElements Before: {totalElements}")
+    while True:
+        params[lastModifiedDate] = date_format_param(lastModifiedDate, diff=hours)
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            if response.status_code == 429:
+                logger.info("Rate limited")                   
+                time.sleep(3600)
+                continue
+            else:
+                logger(f"Error Connecting to API. Params: {params}")
+                break
+        new_elements = response.json()["meta"]["totalElements"]
+        logger.info(f"totalElements After: {new_elements}")
+        if new_elements >= totalElements - 10000:
+            return lastModifiedDate
+        else:
+            hours + 1
+
+        
+        
+
+
 
 def fetch():
     load_dotenv()
     year = 1999
     url = "https://api.regulations.gov/v4/comments"
     output = "comments.csv"
-    start_date = f"0001-01-01T00:00:00"
     ids = set()
 
     print(f"Year: {year}")
@@ -58,9 +83,10 @@ def fetch():
                     break
 
             print(f"MAX COMMENTS: {(response.json())['meta']['totalElements']}")
-        
+            comments = response.json()
+            totalElements = comments["meta"]["totalElements"]
           
-            for comment in ((response.json())["data"]):
+            for comment in (comments["data"]):
                 if (comment["id"] not in ids):
                     writer.writerow([comment["id"],clean_text(comment["attributes"]["title"]),normalize_date(comment["attributes"]["postedDate"]),normalize_date(comment["attributes"]["lastModifiedDate"])])
                     ids.add(comment["id"])
@@ -68,16 +94,15 @@ def fetch():
                     logger.info(f"Duplicate on: {comment['id']}")
                 
             # Handle next page
-            if (response.json())["meta"]["hasNextPage"]:
+            if (comments)["meta"]["hasNextPage"]:
                 page += 1
                 params["page[number]"] = page
                 logger.info(f"Elements left: {response.json()['meta']['totalElements']}")
-            elif (not (response.json())["meta"]["hasNextPage"]) and (response.json())["meta"]["totalElements"] < 10000:
+            elif (not (comments)["meta"]["hasNextPage"]) and (comments)["meta"]["totalElements"] < 10000:
                 break
             else: 
-                print("RESET PARAMETERS")
-                start_date = date_format_param(max(comment["attributes"]["lastModifiedDate"] for comment in (response.json())["data"]))
-                params["filter[lastModifiedDate][ge]"] = start_date
+                logger.info("RESET PARAMETERS")
+                params["filter[lastModifiedDate][ge]"] = test_reset_point(url=url, params=params, lastModifiedDate=max(comment["attributes"]["lastModifiedDate"] for comment in (comments["data"])), totalElements=comments["meta"]["totalElements"])
                 page = 1
                 params["page[number]"] = page
 
