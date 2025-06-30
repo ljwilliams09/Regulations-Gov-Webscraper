12b5b002bd38e57fa8e35ed8b82c89beba8c45c5
Luca Williams <ljwilliams@colgate.edu>
2025-06-30 07:09:07 -0400

    Update year in fetch function to 2025 and simplify output file naming

--- web_scraper/reg_api_v2.py
+++ web_scraper/reg_api_v2.py
@@ -59,63 +59,66 @@ def test_reset_point(url, params, lastModifiedDate, totalElements):
 
 def fetch():
     load_dotenv()
-    year = 2024
+    year = 2025
     url = "https://api.regulations.gov/v4/comments"
-    output = f"{year}_comments.csv"
-    ids_set = set()
-    ids_deque = deque(maxlen=10000)
+    output = f"comments.csv"
 
-    # print(f"Year: {year}")
-    page = 1
-    ge, le = year_range(year)
-    params = {
-                "api_key" : os.getenv("REG_GOV_API_KEY_LW"),
-                "filter[postedDate][ge]" : ge,
-                "filter[postedDate][le]" : le,
-                "sort" : "lastModifiedDate,documentId", 
-                "page[number]" : page,
-                "page[size]" : 250            
-    }
-    with open(output, 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(["id", "title", "postedDate", "lastModifiedDate"])
-        while True:
-            # print(f"Page: {page}")
-            # print(f"Params: {params}")
-                
-            response = requests.get(url, params=params)
-            if response.status_code != 200:
-                if response.status_code == 429:
-                    logger.info("Rate limited")                   
-                    time.sleep(3600)
-                    continue
-                else:
-                    logger.error(f"Error Connecting to API. Params: {params}")
+    while year >= 1800 and year <= 2025:
+        year -= 1
+        logger.info("************ YEAR ***********")
+        ids_set = set()
+        ids_deque = deque(maxlen=10000)
+        # print(f"Year: {year}")
+        page = 1
+        ge, le = year_range(year)
+        params = {
+                    "api_key" : os.getenv("REG_GOV_API_KEY_LW"),
+                    "filter[postedDate][ge]" : ge,
+                    "filter[postedDate][le]" : le,
+                    "sort" : "lastModifiedDate,documentId", 
+                    "page[number]" : page,
+                    "page[size]" : 250            
+        }
+        with open(output, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(["id", "title", "postedDate", "lastModifiedDate"])
+            while True:
+                # print(f"Page: {page}")
+                # print(f"Params: {params}")
+                    
+                response = requests.get(url, params=params)
+                if response.status_code != 200:
+                    if response.status_code == 429:
+                        logger.info("Rate limited")                   
+                        time.sleep(3600)
+                        continue
+                    else:
+                        logger.error(f"Error Connecting to API. Params: {params}")
+                        break
+
+                # print(f"MAX COMMENTS: {(response.json())['meta']['totalElements']}")
+                comments = response.json()
+                totalElements = comments["meta"]["totalElements"]
+            
+                for comment in (comments["data"]):
+                    if (comment["id"] not in ids_set):
+                        writer.writerow([comment["id"],clean_text(comment["attributes"]["title"]),normalize_date(comment["attributes"]["postedDate"]),normalize_date(comment["attributes"]["lastModifiedDate"])])
+                        ids_set, ids_deque = track_id(comment["id"], ids_set, ids_deque)
+                    else:
+                        logger.info(f"Duplicate on: {comment['id']}")
+                    
+                # Handle next page
+                if (comments)["meta"]["hasNextPage"]:
+                    page += 1
+                    params["page[number]"] = page
+                elif (not (comments)["meta"]["hasNextPage"]) and (comments)["meta"]["totalElements"] < 10000:
                     break
-
-            # print(f"MAX COMMENTS: {(response.json())['meta']['totalElements']}")
-            comments = response.json()
-            totalElements = comments["meta"]["totalElements"]
-          
-            for comment in (comments["data"]):
-                if (comment["id"] not in ids_set):
-                    writer.writerow([comment["id"],clean_text(comment["attributes"]["title"]),normalize_date(comment["attributes"]["postedDate"]),normalize_date(comment["attributes"]["lastModifiedDate"])])
-                    ids_set, ids_deque = track_id(comment["id"], ids_set, ids_deque)
-                else:
-                    logger.info(f"Duplicate on: {comment['id']}")
-                
-            # Handle next page
-            if (comments)["meta"]["hasNextPage"]:
-                page += 1
-                params["page[number]"] = page
-                logger.info(f"Elements left: {response.json()['meta']['totalElements']}")
-            elif (not (comments)["meta"]["hasNextPage"]) and (comments)["meta"]["totalElements"] < 10000:
-                break
-            else: 
-                logger.info("RESET PARAMETERS")
-                page = 1
-                params["page[number]"] = page
-                params["filter[lastModifiedDate][ge]"] = test_reset_point(url=url, params=params, lastModifiedDate=max(comment["attributes"]["lastModifiedDate"] for comment in (comments["data"])), totalElements=totalElements)
-        
+                else: 
+                    logger.info(f"Elements left: {response.json()['meta']['totalElements']}")
+                    logger.info("RESET PARAMETERS")
+                    page = 1
+                    params["page[number]"] = page
+                    params["filter[lastModifiedDate][ge]"] = test_reset_point(url=url, params=params, lastModifiedDate=max(comment["attributes"]["lastModifiedDate"] for comment in (comments["data"])), totalElements=totalElements)
+            
 
 fetch()
